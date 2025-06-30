73444dcda7ee21c63cc8f59f92581dc5b9b6bb54
tuongnm <tuongnm@ikameglobal.com>
2025-06-30 21:06:49 +0700

    add winlose script

--- assets/_Game/Script/GameEvent/GameEvent.ts
+++ assets/_Game/Script/GameEvent/GameEvent.ts
@@ -4,4 +4,6 @@ export class GameEvent{
     // static EVENT_GAME_START = "GameStart";
     public static readonly NewItemOnShelf: string = "NewItemOnShelf";
     public static readonly ItemMatched: string = "ItemMatched";
+    public static readonly GameWin: string = "GameWin";
+    public static readonly GameLose: string = "GameLose";
 }
---
+++ assets/_Game/Script/UI/WinLose/WinLoseController.ts
@@ -0,0 +1,41 @@
+import { _decorator, Component, Node } from 'cc';
+import { EventListener } from '../../GameEvent/EventListener';
+import { GameEvent } from '../../GameEvent/GameEvent';
+const { ccclass, property } = _decorator;
+
+@ccclass( 'WinLoseController' )
+export class WinLoseController extends Component
+{
+    @property( Node )
+    winNode: Node = null;
+    @property( Node )
+    loseNode: Node = null;
+
+    static instance: WinLoseController = null;
+
+    protected onEnable (): void
+    {
+        WinLoseController.instance = this;
+        EventListener.on( GameEvent.GameWin, this.onGameWin, this );
+        EventListener.on( GameEvent.GameLose, this.onGameLose, this );
+    }
+
+    protected onDisable (): void
+    {
+        WinLoseController.instance = null;
+        EventListener.off( GameEvent.GameWin, this.onGameWin, this );
+        EventListener.off( GameEvent.GameLose, this.onGameLose, this );
+    }
+
+    onGameWin (): void
+    {
+        this.winNode.active = true;
+    }
+
+    onGameLose (): void
+    {
+        this.loseNode.active = true;
+    }
+}
+
+
